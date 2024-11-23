bfa6dfaf90efd85e6e66f201b6cd1fbf4857b0d1
SrMetus <s.gaggero87@gmail.com>
2024-11-23 17:39:01 -0300

    Modifying attributes, properties and text in HTML.

---
+++ javascript/Manipulacion del DOM/AttributesProperties/app.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('input')
+console.log(input)
+
+console.log(input.value)
+
+console.log(input.value = "Sebastian")
+
+const tittle = document.querySelector("#app-title")
+console.log(tittle)
+
+tittle.textContent = "Hola, Sebastian"
+
+tittle.innerText = "Hola, Gaggero2"
