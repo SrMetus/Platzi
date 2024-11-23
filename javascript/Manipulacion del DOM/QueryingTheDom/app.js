5e0eb9557b0350e6ab78583900e9b6ab40527ea4
SrMetus <s.gaggero87@gmail.com>
2024-11-23 01:37:55 -0300

    Querying the DOM with getElementById and querySelector.

---
+++ javascript/Manipulacion del DOM/QueryingTheDom/app.js
@@ -0,0 +1,9 @@
+//Trae un solo elemento
+document.getElementById('app-id')
+document.querySelector('#app-title')
+document.querySelector('p')
+
+//Trae todos los elementos
+document.getElementsByClassName('menu-items')
+document.getElementsByTagName('p')
+document.querySelectorAll('.menu-items')
